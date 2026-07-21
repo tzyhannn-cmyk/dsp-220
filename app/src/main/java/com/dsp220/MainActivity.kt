dc865f41a833c52fe6f04d1ac6538a6d6de40f81
tzyhannn-cmyk <tzyhannn@gmail.com>
2026-07-21 23:41:35 +0700

    Update MainActivity.kt

--- app/src/main/java/com/dsp220/MainActivity.kt
+++ app/src/main/java/com/dsp220/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.dsp220.pro
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
-import android.webkit.WebSettings
+import android.webkit.WebSettings // PEMBARUAN: Import library pengaturan web
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -42,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             @Suppress("DEPRECATION")
             allowUniversalAccessFromFileURLs = true
             
-            // Mengizinkan HTML lokal memproses & menyuarakan audio dari HTTPS internet
+            // PERBAIKAN UTAMA: Mengizinkan HTML lokal memproses & menyuarakan audio dari HTTPS internet
             mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         }
 
@@ -104,34 +103,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class AndroidBridge {
-
-        // --- FITUR BARU: Menjalankan Background Playback Service ---
-        @JavascriptInterface
-        fun startBackgroundService() {
-            try {
-                val serviceIntent = Intent(this@MainActivity, PlaybackService::class.java)
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    startForegroundService(serviceIntent)
-                } else {
-                    startService(serviceIntent)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        // --- FITUR BARU: Menghentikan Background Playback Service ---
-        @JavascriptInterface
-        fun stopBackgroundService() {
-            try {
-                val serviceIntent = Intent(this@MainActivity, PlaybackService::class.java)
-                stopService(serviceIntent)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        // --- FUNGSI ASLI UNTUK EKSTRAKSI YOUTUBE ---
         @JavascriptInterface
         fun extractYouTubeAudio(url: String) {
             Thread {
